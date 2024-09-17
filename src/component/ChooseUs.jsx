615c3eaf343dc6dd343359245b9a386d97dbdb53
OMSAH <huncyoms@gmail.com>
2024-09-17 01:21:10 -0400

    saved

--- src/component/ChooseUs.jsx
+++ src/component/ChooseUs.jsx
@@ -3,15 +3,17 @@ import { Box, Typography } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import ShieldIcon from "@mui/icons-material/Shield";
 import PriceCheckIcon from "@mui/icons-material/PriceCheck";
+import LocationCityIcon from "@mui/icons-material/LocationCity";
+import CalculateIcon from "@mui/icons-material/Calculate";
 import { styled } from "@mui/system"; // Correct import
 import Slider from "react-slick"; // Import the carousel
 
 const Container = styled(Box)({
   height: "130px",
-  width:"100%",
+  width: "100%",
   // border:'2px solid red',
   "@media (max-width:700px)": {
-    height:"100px",
+    height: "100px",
   },
 });
 
@@ -19,16 +21,23 @@ const Container = styled(Box)({
 const settings = {
   infinite: true,
   speed: 1000,
-  slidesToShow: 3,
+  slidesToShow: 4,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 3000,
-  rtl:true,
+  rtl: true,
   responsive: [
     {
       breakpoint: 700, // For screens up to 768px wide
       settings: {
         slidesToShow: 2, // Show only 1 slide
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 1000, // For screens up to 768px wide
+      settings: {
+        slidesToShow: 3, // Show only 1 slide
         slidesToScroll: 1,
       },
     },
@@ -59,6 +68,19 @@ const ChooseUs = () => {
             <PriceCheckIcon sx={{ fontSize: 30, color: "#000" }} />
             <Typography sx={{ marginTop: 1, fontSize: 16, color: "#000" }}>
               Transparent pricing
+            </Typography>
+          </Box>
+          <Box textAlign="center">
+            <LocationCityIcon sx={{ fontSize: 30, color: "#000" }} />
+            <Typography sx={{ marginTop: 1, fontSize: 16, color: "#000" }}>
+              600+ <br />
+              In-house Designer
+            </Typography>
+          </Box>
+          <Box textAlign="center">
+            <CalculateIcon sx={{ fontSize: 30, color: "#000" }} />
+            <Typography sx={{ marginTop: 1, fontSize: 16, color: "#000" }}>
+              Easy EMIs
             </Typography>
           </Box>
         </Slider>
