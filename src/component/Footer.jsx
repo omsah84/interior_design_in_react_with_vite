839af5e4a097a5f0165d8e207ab8fe8807668f87
OMSAH <huncyoms@gmail.com>
2024-09-25 04:09:17 -0400

    added

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,5 +1,4 @@
-// import React from 'react';
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Link } from "@mui/material";
 import { styled } from "@mui/system";
 
 const FooterContainer = styled(Box)({
@@ -8,35 +7,64 @@ const FooterContainer = styled(Box)({
   textAlign: "center",
 });
 
-// const Container = styled(Box)({
-//   display: "flex",
-//   "@media (max-width: 700px)": {
-//     flexDirection: "column",
-//     gap: "20px",
-//   },
-// });
-// const FooterLink = styled(Link)({
-//   margin: "10 10px",
-//   color: "#000",
-//   textDecoration: "none",
-//   textAlign: "center",
-//   fontSize: "10px",
-//   flexBasis: "100%",
+const Container = styled(Box)({
+  display: "flex",
+  justifyContent: "space-around", // Space items evenly in a single row
+  alignItems: "center", // Center items vertically
+  flexWrap: "wrap", // Allow wrapping on smaller screens
+  marginBottom: "30px",
+  "@media (max-width: 700px)": {
+    flexDirection: "column",
+    alignItems: "center", // Center items on smaller screens
+  },
+});
 
-//   "&:hover": {
-//     textDecoration: "underline",
-//   },
-// });
+const FooterLink = styled(Link)({
+  margin: "0 10px",
+  color: "#000",
+  textDecoration: "none",
+  fontSize: "14px", // Adjusted font size for better visibility
+  "&:hover": {
+    textDecoration: "underline",
+  },
+});
+
+const Address = styled(Typography)({
+  margin: "10px 0", // Margin for spacing
+  fontSize: "14px", // Adjusted font size
+  color: "#555", // Slightly darker color for contrast
+  textAlign: "center", // Center the address text
+});
 
 const Footer = () => {
   return (
     <FooterContainer>
-      {/* <Container m={3}>
-        <FooterLink href="#">Interior designers in Pune</FooterLink>
-        <FooterLink href="#">Interior designers in Kolkata</FooterLink>
-        <FooterLink href="#">Interior designers in Gurgaon</FooterLink>
-      </Container> */}
-      <Typography variant="body4" color="textSecondary">
+      <Container m={3}>
+        {/* Menu Links */}
+        <Box display="flex" flexDirection={{ xs: "column", sm: "column" }}>
+          <FooterLink href="#">Our Services</FooterLink>
+          <FooterLink href="#">Contact Us</FooterLink>
+          <FooterLink href="#">About Us</FooterLink>
+        </Box>
+        <Box display="flex" flexDirection={{ xs: "column", sm: "column" }}>
+          <FooterLink href="#">Interior Designers in Pune</FooterLink>
+          <FooterLink href="#">Interior Designers in Kolkata</FooterLink>
+          <FooterLink href="#">Interior Designers in Gurgaon</FooterLink>
+        </Box>
+
+        {/* Address Section */}
+        <Address>
+          123 Design Street, Suite 456
+          <br />
+          Pune, Maharashtra, 411001
+          <br />
+          Phone: (123) 456-7890
+          <br />
+          Email: contact@example.com
+        </Address>
+      </Container>
+
+      <Typography variant="body2" color="textSecondary">
         Copyright © 2023. All rights reserved.
       </Typography>
     </FooterContainer>
